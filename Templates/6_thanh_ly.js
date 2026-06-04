870ed9164b5755151e9350354bd89eedb2690706
cuongnm365-ai <cuongnm365@gmail.com>
2026-06-05 01:34:26 +0700

    Update 6_thanh_ly.js

--- Templates/6_thanh_ly.js
+++ Templates/6_thanh_ly.js
@@ -17,12 +17,12 @@ window.SOC_TEMPLATES["t_thanh_ly"] = {
             <li style="margin-bottom: 4px;"><b>Mã khách hàng:</b> <b>{{contractId}}</b></li>
             <li style="margin-bottom: 4px;"><b>Địa chỉ sử dụng:</b> <b>{{address}}</b></li>
         </ul>
-        <p style="margin-bottom:0; font-style: italic;">➔ Chi tiết hợp đồng, biên bản điện tử vui lòng xem tại ứng dụng <b>Hi FPT</b> (quét hoặc chạm mã QR bên cạnh, đăng nhập bằng SĐT <b>{{phone}}</b>).</p>
+        <p style="margin-top: 10px; margin-bottom: 0; font-style: italic;">➔ Chi tiết hợp đồng, biên bản điện tử vui lòng xem tại ứng dụng <b>Hi FPT</b> (quét hoặc chạm mã QR bên cạnh, đăng nhập bằng SĐT <b>{{phone}}</b>).</p>
     `,
     
     body: `
         Kính chào {{honorific}} <b>{{customerName}}</b>,<br><br>
-        Em là <b>{{staffName}}</b> – CSKH FPT Telecom.<br><br>
+        Em là <b>{{staffName}}</b> – Nhân viên CSKH FPT Telecom.<br><br>
         Em hiểu và chia sẻ với những thông tin mà {{pronoun}} cung cấp liên quan tới hợp đồng dịch vụ Internet <b>{{contractId}}</b> tại địa chỉ <b>{{address}}</b>.<br><br>
         Như có trao đổi với {{pronoun}}, hiện bên em sẽ phối hợp cùng phía phụ trách hợp đồng để có phương án phục vụ phù hợp nhất với yêu cầu của {{pronounLc}}. Trong thời gian này, em xin phép gửi lại thông tin hợp đồng để {{pronounLc}} tham khảo và cân nhắc ạ:<br>
         {INFO_BOX}
